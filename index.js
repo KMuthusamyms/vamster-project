89e1c78b47d5d96201a75f350ef97518e58a03dc
KMuthusamyms <k.muthusamyms@gmail.com>
2021-01-24 15:01:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require("puppeteer");
+const BASE_URL =
+  "https://prefeitura.pbh.gov.br/saude/licitacao/pregao-eletronico-151-2020";
+const extractDetails = async () => {
+  let browser = await puppeteer.launch({ headless: false });
+  let page = await browser.newPage();
+  //page.on("console", (consoleObj) => console.log(consoleObj.text()));
+  await page.goto(BASE_URL, { waitUntil: "networkidle0", timeout: 90000 });
+  await browser.close();
+};
+extractDetails();
